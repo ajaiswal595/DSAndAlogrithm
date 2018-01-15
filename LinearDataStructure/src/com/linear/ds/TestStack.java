156ea3e1a7a026c3970bc50fa765cfaed5a924f3
Ajayj <ajaiswal595@gmail.com>
2018-01-15 21:58:47 +0530

    added Adjucency Graph

--- LinearDataStructure/src/com/linear/ds/TestStack.java
+++ LinearDataStructure/src/com/linear/ds/TestStack.java
@@ -1,5 +1,7 @@
 package com.linear.ds;
 
+import com.nonlinear.ds.Graph;
+
 public class TestStack {
 
 	public static void main(String[] args) {
@@ -63,7 +65,7 @@ public class TestStack {
 		System.out.println(prashant);*/
 		
 		
-		System.out.println(map.get("0126ca121001"));
+	/*	System.out.println(map.get("0126ca121001"));
 		System.out.println(map.get("0126ca121002"));
 		System.out.println(map.get("0126ca121003"));
 		
@@ -75,6 +77,21 @@ public class TestStack {
 		System.out.println(map.get("0126ca121001"));
 		System.out.println(map.get("0126ca121002"));
 		System.out.println(map.get("0126ca121003"));
+		
+		*/
+		
+		Graph graph=new Graph(5);
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 4);
+		graph.addEdge(1, 2);
+		graph.addEdge(1, 3);
+		graph.addEdge(1, 4);
+		graph.addEdge(2, 3);
+		graph.addEdge(3, 4);
+		
+		graph.printGraph();
+		
+		
 	}
 
 }
